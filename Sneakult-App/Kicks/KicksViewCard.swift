e4e7f6819895b9199fc7428e39504cd2931a75fc
Jipnesh Kumar <140158559+Jipnesh-07@users.noreply.github.com>
2024-05-10 10:06:50 +0530

    Bidding Socket integration - minor ui updates across app

---
+++ Sneakult-App/Kicks/KicksViewCard.swift
@@ -0,0 +1,18 @@
+//
+//  KicksViewCard.swift
+//  Sneakult-App
+//
+//  Created by STUDENT on 08/05/24.
+//
+
+import SwiftUI
+
+struct KicksViewCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KicksViewCard()
+}
